3d2f558811d8961faf6f2b9c6405bf297a3a9cad
Jeremy Corson <jcorson@dekaresearch.com>
2015-09-10 13:45:18 -0400

    useer can signup and some basic navigation in place. need to really think about how i want to manage routes but i will follow allong for now

--- app/routes.js
+++ app/routes.js
@@ -11,7 +11,7 @@ function getRecipes(res){
 		});
 };
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
 
 	// api ---------------------------------------------------------------------
 	// get all recipes
@@ -50,9 +50,61 @@ module.exports = function(app) {
 		});
 	});
 
+	// =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.sendfile('./public/js/login/login.tmpl.html');
+    });
+
+     app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.sendfile('./public/js/login/signup.tmpl.html');
+    });
+
+	// process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.sendfile('./public/js/profile/profile.tmpl.html');
+    });
+
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
 		console.log('Application Router: Catch all fired. Returning: ./public' + req.originalUrl);
 		res.sendfile('./public' + req.originalUrl); // load the single view file (angular will handle the page changes on the front-end)
 	});
+
+	// route middleware to make sure a user is logged in
+	function isLoggedIn(req, res, next) {
+
+	    // if user is authenticated in the session, carry on
+	    if (req.isAuthenticated()) {
+	        return next();
+	    }
+
+	    // if they aren't redirect them to the home page
+	    res.redirect('/');
+	}
 };
