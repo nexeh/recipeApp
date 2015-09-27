c50f70fbf512f6313b6183bb591abe34b7bca752
Jeremy Corson <jeremy.r.corson@gmail.com>
2015-09-27 11:22:40 -0400

    made instructions and ingredients of the recipe form data driven.

--- public/js/recipe/recipe.controller.js
+++ public/js/recipe/recipe.controller.js
@@ -3,7 +3,21 @@ angular.module('recipeApp.recipe.recipeController', [])
 .controller('recipeController', ['$scope', function($scope) {
 
 	$scope.recipeForm = {
-		ingredients: [{}]
+		ingredients: [{},{}],
+		instructions: [{}]
+	};
+
+	$scope.addIngredient = function () {
+		$scope.recipeForm.ingredients.push({});
+	};
+
+	$scope.addInstruction = function () {
+		$scope.recipeForm.instructions.push({});
+	};
+
+
+	$scope.saveRecipe = function () {
+		console.log($scope.recipeForm);
 	};
 
 }]);
