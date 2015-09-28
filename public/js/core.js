75bd2f1b6d7b3a72e91164bb36097ca1bbf8a41b
Jeremy Corson <jcorson@dekaresearch.com>
2015-09-28 14:12:25 -0400

    added some blog and merketing pages but need to get teh latest code from imac to integrate the rest.

--- public/js/core.js
+++ public/js/core.js
@@ -1,7 +1,8 @@
 angular.module('recipeApp', [
-	'recipeApp.recipe', 
-	'recipeApp.recipe.recipeService', 
+	'recipeApp.recipe',
+	'recipeApp.recipe.recipeService',
 	'recipeApp.recipe.recipeController',
-	'recipeApp.recipe.ingredient.ingredientController', 
-	'recipeApp.recipe.ingredient.ingredientDirective'
+	'recipeApp.recipe.ingredient.ingredientController',
+	'recipeApp.recipe.ingredient.ingredientDirective',
+    'recipeApp.routes'
 ]);
