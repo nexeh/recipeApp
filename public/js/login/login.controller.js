c0b8483d397352884f41853a9ec1407fffa1b0ea
Jeremy Corson <Jeremy.r.corson@gmail.com>
2015-10-03 20:14:04 -0400

    In flight changes on my mac book around the login

---
+++ public/js/login/login.controller.js
@@ -0,0 +1,21 @@
+angular.module('recipeApp.login.loginController', ['recipeApp.login.loginService'])
+
+.controller('loginController', 
+	['$scope', '$http', 'LoginService', function($scope, $http, LoginService) {
+
+	$scope.loginForm = {};
+
+	$scope.login = function () {
+
+		console.log('login controller: login function');
+
+		LoginService.login($scope.loginForm)
+		    .success(function(data) {
+		        console.log(data);
+		        $scope.loginForm = {};
+		    	$scope.isLoggedIn = true;
+		    	$scope.user = data;
+		    });
+	};
+
+}]);
